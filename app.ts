c9c27ac59107eb70836883730dd2fb3f0314dfd7
Kleiton Marques <kleiton.88design@gmail.com>
2018-12-02 14:25:32 -0200

    Declarando váriaveis

---
+++ app.ts
@@ -0,0 +1,13 @@
+let message: string = "Help me, Obi-Wan Kenobi. You're my only hope!"
+console.log(message)
+
+let episode: number = 4
+episode = '4'
+console.log("This is episode " + 4)
+episode = episode + 1
+console.log("Next episode is " + episode)
+
+let favoriteDroid
+favoriteDroid = 'BB-8'
+favoriteDroid = 10
+console.log("My favorite droid is " + favoriteDroid)
