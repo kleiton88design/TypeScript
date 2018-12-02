818234641bb3d83c93d5b5ff0c833c7f53b6b7db
Kleiton Marques <kleiton.88design@gmail.com>
2018-12-02 15:22:53 -0200

    Usando funcoes

--- app.ts
+++ app.ts
@@ -11,3 +11,20 @@ let favoriteDroid
 favoriteDroid = 'BB-8'
 favoriteDroid = 10
 console.log("My favorite droid is " + favoriteDroid)
+
+let isEnoughToBeatMF = function (parsecs: number): boolean {
+	return parsecs < 12
+}
+
+let distance = 11
+console.log(`is ${distance} parsecs enough to beat Millenium Falcon? ${isEnoughToBeatMF(distance) ? 'YES': 'NO'} `)
+
+let call = (name: string) => console.log(`Do you copy, ${name}?`)
+call('R2')
+
+function inc (speed: number, inc: number = 1) : number {
+	return speed + inc
+}
+
+console.log(`inc (5, 1) = ${inc(5, 1)}`)
+console.log(`inc (5) = ${inc(5)}`)
